c64444417dce709d3f67fb9dcfbc3e4784b1768a
0574800076 <stephanyleboss@gmail.com>
2026-02-23 18:29:19 +0000

    Add stylizedCardMessage function for card messages

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+mport fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "Digix Crew",
+        body: "𓆩 BRICO BOT 𓆪",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
